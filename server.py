f3f53f1a5f84a74ec1f56d81b746feb0a8a80d49
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-19 13:04:49 -0700

    added templates, log in, and register

--- server.py
+++ server.py
@@ -2,10 +2,13 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, jsonify)
+
+from model import User, Rating, Movie, connect_to_db, db
+
 
 
 app = Flask(__name__)
@@ -22,8 +25,72 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    a = jsonify([1, 3])
+    return render_template('homepage.html')
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("all_users.html", users=users)
+
+
+@app.route("/register")
+def reg_form():
+    """Show registration form"""
+
+    return render_template("registration_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def confirm_registration():
+    """Confirms registration"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    duplicates = db.session.query(User).filter_by(email=email).all()
+
+    if duplicates:
+        flash("This email is already registered. Please try again with a different email.")
+    else:
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("You have been registered! Yay!")
+
+    return redirect("/")
+
+
+@app.route("/login", methods=["POST"])
+def log_in():
+    """Logs a user in"""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    duplicates = db.session.query(User).filter_by(email=email).first()
+
+    if duplicates:
+        if password == duplicates.password:
+            session[email] = password
+            flash("You have been logged in!")
+            return redirect("/")
+        else:
+            flash("Wrong password. Please try again.")
+            return redirect("/")
+    else:
+        flash("Not a valid user. Please create an account.")
+        return render_template("registration_form.html")
+
+@app.route("/logout")
+def log_out():
+    """Logs the user out"""
+
+    for key in session.keys():
+     session.pop[]
 
 
 if __name__ == "__main__":
@@ -37,6 +104,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-
-    
     app.run(port=5000, host='0.0.0.0')
